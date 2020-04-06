4e5f12a6c39199922babc387bbc0cfee88ba853a
Andrew <smigaja@gmail.com>
2020-04-06 16:07:37 -0700

    Finished single point demo

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -8,13 +8,18 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+//let mapStyle = "streets-v11";
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{mapStyle}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	//id: 'mapbox.pirates',
+  maxZoom: 18,
+  mapStyle: "streets-v11",
 	accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
+
 streets.addTo(map);
 
+L.circleMarker([34.0522, -118.2437], 
+          { radius: 300,
+            color: "black" }
+        ).addTo(map);

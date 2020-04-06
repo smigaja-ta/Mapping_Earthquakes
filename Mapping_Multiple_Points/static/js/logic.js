0435a2e4fbd000345133d3be2163f4ff7e8eedd8
Andrew <smigaja@gmail.com>
2020-04-06 16:12:34 -0700

    Adding multiple points initial files and directories

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,25 @@
+console.log("working");
+
+
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
+
+//let mapStyle = "streets-v11";
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{mapStyle}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  mapStyle: "streets-v11",
+	accessToken: API_KEY
+});
+
+streets.addTo(map);
+
+L.circleMarker([34.0522, -118.2437], 
+          { radius: 300,
+            color: "black" }
+        ).addTo(map);
